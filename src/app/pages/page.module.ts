b73431f19cd3193a7bb9875db9a766d85136d72b
114588-Luis Ivetta <114588@tecnicatura.frc.utn.edu.ar>
2024-04-01 08:38:46 -0300

    header side y breadcrumbs

---
+++ src/app/pages/page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+
+@NgModule({
+    imports: [],
+    exports: [],
+    declarations: [
+    DashboardComponent
+  ],
+    providers: [],
+})
+export class PageModule { }
+
