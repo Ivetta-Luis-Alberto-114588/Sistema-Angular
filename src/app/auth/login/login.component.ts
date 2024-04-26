d66c01341624612f2f8f60942809ec46cbb6972c
114588-Luis Ivetta <114588@tecnicatura.frc.utn.edu.ar>
2024-04-26 20:20:25 -0300

    recuerdame y google token frontend

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { loginFormInterface } from 'src/app/interfaces/loginForm.interface';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import Swal from 'sweetalert2';
+
+
+declare const google: any;
 
 @Component({
   selector: 'app-login',
@@ -11,14 +14,17 @@ import Swal from 'sweetalert2';
   styleUrls: ['./login.component.css' ]
 })
 
-export class LoginComponent {
+
+export class LoginComponent implements AfterViewInit {
+
+  @ViewChild('googleBtn') googleBtn! : ElementRef;
 
   public formSubmitted = false
 
   public loginForm = this.fb.group({
 
-    email: ['laivetta@gmail.com', [ Validators.email, Validators.required ] ],
-    password: ['123', [ Validators.required]],
+    email: [localStorage.getItem('email') || '', [ Validators.email, Validators.required ] ],
+    password: ['', [ Validators.required]],
     remember: [false]    
   })
   
@@ -29,6 +35,31 @@ export class LoginComponent {
     private usuarioService: UsuarioService
   ){}
 
+
+  ngAfterViewInit(): void {
+
+   this.googleInit()
+
+  }
+
+
+  googleInit(){
+    google.accounts.id.initialize({
+      client_id: "1098957435153-cjhs40btr7pg9iutn6d24f45kg8448d9.apps.googleusercontent.com",
+      callback: this.handleCredentialResponse
+    });
+
+    google.accounts.id.renderButton(
+      this.googleBtn.nativeElement,
+      // document.getElementById("buttonDiv"),
+      { theme: "outline", size: "large" }  // customization attributes
+    );
+  }
+
+  handleCredentialResponse( response: any ){
+    console.log("Encoded JWT ID token: " + response.credential);
+  }
+
   login()  {
     // console.log(this.loginForm.value)
     //puedo guardar el token aca en el metodo next o directamente en el servicio
@@ -37,6 +68,14 @@ export class LoginComponent {
       next: (data: any) => {  console.log(data)
         // Swal.fire("info",data.token, "info"),
         // localStorage.setItem('token', data.token)
+
+        //me fijo si el remember es true entonces guardo el mail en el localstorage
+        if(this.loginForm.get('remember')?.value){
+          localStorage.setItem('email', this.loginForm.get('email')?.value || '' )
+        } else {
+          localStorage.removeItem('email')
+        }
+
         },
       error: e => { Swal.fire("error", e.error.msg, "error") }
     })
