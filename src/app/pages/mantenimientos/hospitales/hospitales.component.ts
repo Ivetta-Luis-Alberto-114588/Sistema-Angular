152fbfeeacafd5c8d34714deef714261d51ef380
114588-Ivetta luis <114588@tecnicatura.frc.utn.edu.ar>
2024-05-19 14:08:39 -0300

    tabla hospitales

--- src/app/pages/mantenimientos/hospitales/hospitales.component.ts
+++ src/app/pages/mantenimientos/hospitales/hospitales.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hospital } from 'src/app/models/hospital.model';
 import { HospitalService } from 'src/app/services/hospital.service';
 
 @Component({
@@ -9,13 +10,23 @@ import { HospitalService } from 'src/app/services/hospital.service';
 })
 export class HospitalesComponent implements OnInit{
 
+  public lista_hospitales : Hospital[] = []
+  public cargando : boolean = true
+
   constructor(private hospitalService: HospitalService){}
   
   ngOnInit(): void {
-    
+    this.cargarHospital()    
+  }
+
+
+  
+  cargarHospital(){
+    this.cargando = true
     this.hospitalService.cargarHospitales()
       .subscribe( resp => {
-        console.log( resp)
+        this.cargando = false
+        this.lista_hospitales = resp
       })
 
   }
