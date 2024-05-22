8d23a4eb02e27f2dbb4ff1383a5ded4429bfd6ba
114588-Luis Ivetta <114588@tecnicatura.frc.utn.edu.ar>
2024-05-22 13:06:27 -0300

    medico hospital

--- src/app/pages/mantenimientos/medicos/medico/medico.component.ts
+++ src/app/pages/mantenimientos/medicos/medico/medico.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Hospital } from 'src/app/models/hospital.model';
+import { HospitalService } from 'src/app/services/hospital.service';
 
 @Component({
   selector: 'app-medico',
   templateUrl: './medico.component.html',
   styleUrls: ['./medico.component.css']
 })
-export class MedicoComponent {
+export class MedicoComponent implements OnInit {
+  
+  public formMedico! : FormGroup;
+  public lista_hospitales: Hospital[]= []
+  public hospitalSeleccionado! : Hospital
+  
+  ngOnInit(): void {
+    
+    this.formMedico = this.fb.group({
+      nombre : ['luis', Validators.required],
+      hospital: ["", Validators.required]
+    })
+
+    this.cargarHospitales()
+
+    this.formMedico.get('hospital')?.valueChanges.subscribe(
+      hosp_id => {
+       this.hospitalSeleccionado = this.lista_hospitales.find(x => x._id === hosp_id) as Hospital
+       
+      } )
+    
+      
+  }
+
+  constructor( private fb: FormBuilder, private hospitalService: HospitalService){}
+
+  guardarMedico(){
+    console.log(this.formMedico)
+  }
+
+  cargarHospitales(){
+    this.hospitalService.cargarHospitales().subscribe(
+      resp => {
+        this.lista_hospitales = resp
+      }
+    )
+  }
+  
+
 
 }
